c77dd9991b48cdd0ce0f160264af760c189f0f84
Drew Blasius <drewblasius@gmail.com>
2018-02-10 15:06:51 -0600

    add in .tmux.conf

--- dots.sh
+++ dots.sh
@@ -3,5 +3,9 @@
 [[ $USER == root ]] && exit;
 
 # put the dotfiles in the right place
+echo 'adding .vimrc'
 cp -f ./.vimrc ~/.vimrc
+echo 'adding .gitconfig'
 cp -f ./.gitconfig ~/.gitconfig
+echo 'adding .tmux.conf'
+cp -f ./.tmux.conf ~/.tmux.conf
